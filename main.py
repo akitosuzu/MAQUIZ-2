fef95b6fff6a7ae240efe4da7b1a30bf0c03c631
AkitoSuzuki1 <88513410+akitosuzu@users.noreply.github.com>
2021-09-20 11:39:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("\033[1;32;40m  \n")
+
+
+#imports
+import random
+import time
+
+
+#Variables
+points = 0 
+correct_answers = 0
+wrong_answers = 0
+rounds_played = 0
+first_number = 0
+second_number = 0
+
+  
